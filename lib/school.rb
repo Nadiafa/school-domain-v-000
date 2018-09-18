ca3790b8b18bad1f21c71268c5427f5f736a4a49
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-18 13:44:06 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,3 +22,21 @@ class School
   end
 end
 
+
+
+
+
+
+class Album
+  attr_accessor :title, :author, :release_year
+  @@all = []
+  def initialize(title, author, release_year)
+    @title = title
+    @author = author
+    @release_year = release_year
+    @@all << @title
+  end
+end
+black_sunday=Album.new("Black Sunday", "Cypress Hill", 1993)
+temples_of_boom=Album.new("Temples of Boom", "Cypress Hill", 1995)
+skull_and_bones=Album.new("Skull & Bones", "Cypress Hill", 2000)
