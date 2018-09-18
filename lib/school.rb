25562650b0b7b5e35376417856682f21e140d9ef
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-18 13:23:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,24 @@
-# class School
-#   attr_reader :name, :roster
-#
-#   def initialize(name)
-#     @name = name
-#     @roster = {}
-#   end
-#
-#   def add_student(student, grade)
-#     roster[grade] ||= []
-#     roster[grade] << student
-#   end
-#
-#   def grade(grade)
-#     @roster[grade]
-#   end
-#
-#   def sort
-#     sorted = {}
-#     roster.each { |grade, student| sorted[grade] = student.sort }
-#     sorted
-#   end
-# end
-#
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each { |grade, student| sorted[grade] = student.sort }
+    sorted
+  end
+end
+
